258596e70654b1a87d57ac14ec98a5637d599a57
Muhamad Fadli Sirojudin <mfadlisirojudin@gmail.com>
2025-06-17 01:26:42 +0700

    add lihat penilaian dan status

--- src/components/fragments/TableSiswa.tsx
+++ src/components/fragments/TableSiswa.tsx
@@ -1,9 +1,11 @@
 import toast from "react-hot-toast";
 import api from "../../libs/axios";
 import { Siswa } from "../../types";
+import { useNavigate } from "react-router-dom";
 
 const TableSiswa = ({ data, fetch }: { data: Siswa[]; fetch: () => void }) => {
   const guruId = localStorage.getItem("guruId");
+  const navigate = useNavigate(); // ✅ Tambahkan ini
   const deleteSiswa = async (id: number) => {
     try {
       const { data } = await api.delete(`/siswa/${id}?guruId=${guruId}`);
@@ -25,11 +27,12 @@ const TableSiswa = ({ data, fetch }: { data: Siswa[]; fetch: () => void }) => {
         <table className="min-w-full bg-white border border-gray-200 rounded shadow">
           <thead className="bg-blue-900 text-white">
             <tr>
-              <th className="px-4 py-3 w-[10%] text-center">No</th>
-              <th className="px-4 py-3 text-left">Nama</th>
-              <th className="px-4 py-3 text-left">Semester</th>
-              <th className="px-4 py-3 text-left">Kelompok</th>
-              <th className="px-4 py-3 w-[10%]"></th>
+              <th className="px-4 py-3 w-[10%] border text-center">No</th>
+              <th className="px-4 py-3 border text-center">Nama</th>
+              <th className="px-4 py-3 border text-center">Semester</th>
+              <th className="px-4 py-3 border text-center">Kelompok</th>
+              <th className="px-4 py-3 border text-center">Status</th>
+              <th className="px-4 py-3 border text-center">Aksi</th>
             </tr>
           </thead>
           <tbody>
@@ -38,20 +41,29 @@ const TableSiswa = ({ data, fetch }: { data: Siswa[]; fetch: () => void }) => {
                 key={index}
                 className="border-b hover:bg-blue-50 p-4 transition"
               >
-                <td className="px-4 py-2 text-center">{index + 1}</td>
-                <td className="px-4 py-2">{siswa.nama}</td>
-                <td className="px-4 py-2">{siswa.semester}</td>
-                <td className="px-4 py-2">{siswa.kelompok}</td>
-                <td>
-                  <button
-                    className="px-4 py-2 text-red-700 cursor-pointer"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      deleteSiswa(siswa.id);
-                    }}
-                  >
-                    Hapus
-                  </button>
+                <td className="px-4 py-2 border text-center">{index + 1}</td>
+                <td className="px-4 py-2 border">{siswa.nama}</td>
+                <td className="px-4 py-2 border">{siswa.semester}</td>
+                <td className="px-4 py-2 border">{siswa.kelompok}</td>
+                <td className="px-4 py-2 border text-center">{siswa.totalNilai && siswa.totalNilai > 0 ? "✅" : "❌"}</td>
+                <td className="px-4 py-2 border">
+                  <div className="flex justify-center items-center gap-2">
+                    <button
+                      className="text-sm text-white cursor-pointer bg-red-500 hover:bg-red-600 px-3 py-1 rounded"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        deleteSiswa(siswa.id);
+                      }}
+                    >
+                      Hapus
+                    </button>
+                    <button
+                      className="bg-green-600 text-white px-4 py-1 rounded hover:bg-green-700 cursor-pointer"
+                      onClick={() => navigate(`/penilaian/${siswa.id}`)}
+                    >
+                      Lihat Penilaian
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
--- src/types.ts
+++ src/types.ts
@@ -18,6 +18,7 @@ export type Siswa = {
   nama: string;
   semester: number;
   kelompok: string;
+  totalNilai: number;
 };
 
 export type Pembelajaran = {
